5da092ec1465cdc7c036164231b1edc8012cf998
ArtemPustoutov <138291062+ArtemPustoutov@users.noreply.github.com>
2023-07-08 19:30:27 +0300

    Delete index.js

--- lines/index.js
+++
@@ -1,7 +0,0 @@
-let x= prompt('Как дела?');
-let y= prompt('как погода?');
-let z= prompt('Ваш Город?');
-alert(`Итак : дела ${x}, погода ${y}, Вы из : ${z}`);
-let name= prompt('Ваше имя?')
-let ege= prompt('Сколько вам лет?');
-alert (`Вітаю ${name} \nВам ${ege} лет`);
