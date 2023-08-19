4abff9f374bcfde4eb436f11e94756792976d68f
ArtemPustoutov <artem.pus89@gmail.com>
2023-08-19 19:53:37 +0300

    table

---
+++ table/index.js
@@ -0,0 +1,12 @@
+let table = document.createElement('table');
+
+for(let i = 0; i < 10; i++) {
+    let tr = document.createElement('tr');
+    for(let i = 0; i < 10; i++) {
+        let td = document.createElement('td');
+        td.innerHTML = Math.floor(Math.random() * 100) + 1;
+        tr.appendChild(td);
+    }
+    table.appendChild(tr);
+}
+document.body.appendChild(table);
