280b79743ff4db3511c53bec52fd6372eaea31a2
ArtemPustoutov <artem.pus89@gmail.com>
2023-09-12 13:29:50 +0300

    generatelist retake

--- generateList/index.js
+++ generateList/index.js
@@ -1,22 +1,41 @@
-let array = [1, 2, [1.1, 1.2, 1.3], 3];
+// let array = [1, 2, [1.1, 1.2, 1.3], 3];
  
-generateList(array);
+// generateList(array);
 
-function generateList(array) {
-    let str = "<ul>";
-    for (let i = 0; i < array.length; i++) {
-        let row = array[i];
-        if (Array.isArray(row)) {
-            str += "<li><ul>";
-            for (let j = 0; j < row.length; j++) {
-                str += `<li>${row[j]}</li>`;
-            }
-            str += "</ul></li>";
+// function generateList(array) {
+//     let str = "<ul>";
+//     for (let i = 0; i < array.length; i++) {
+//         let row = array[i];
+//         if (Array.isArray(row)) {
+//             str += "<li><ul>";
+//             for (let j = 0; j < row.length; j++) {
+//                 str += `<li>${row[j]}</li>`;
+//             }
+//             str += "</ul></li>";
+//         }
+//         else {
+//             str += `<li>${row}</li>`;
+//         }
+//     }
+//     str += "</ul>";
+//     document.body.innerHTML = str;
+// }
+let array = [1, 2, [1.1,[6, 7, 8] , 1.3], 3];
+
+let generateList = function generateList(array) {
+    let ul = document.createElement('ul');
+    array.forEach(function(element) {
+        let childElement;
+        let li = document.createElement('li');
+        if(Array.isArray(element)) {
+            childElement = generateList(element);
+        }else {
+            childElement = document.createTextNode(element);
         }
-        else {
-            str += `<li>${row}</li>`;
-        }
-    }
-    str += "</ul>";
-    document.body.innerHTML = str;
+        li.appendChild(childElement);
+        ul.appendChild(li)
+    });
+    return ul;
 }
+
+document.body.appendChild(generateList(array));
