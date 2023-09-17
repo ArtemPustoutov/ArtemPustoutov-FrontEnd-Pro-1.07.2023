c136ef95a74ff711d0939ae1c66f9aa9de175328
ArtemPustoutov <artem.pus89@gmail.com>
2023-09-17 16:09:39 +0300

    localStorage

---
+++ localStorage/index.js
@@ -0,0 +1,103 @@
+let data =  {
+    drink: [
+        {id:'A441',name:'Pepsi',category:'Soft Drink', price:15, quantity:2},
+        {id:'B234',name:'Coke', category:'Soft Drink', price:10, quantity:8},
+        {id:'A617',name:'Mirinda', category:'Soft Drink', price:20, quantity:20},
+        {id:'B003',name:'Sprite', category:'Soft Drink', price:16, quantity:5},
+        {id:'B225',name:'Minute Maid', category:'Soft Drink', price:25, quantity:12},
+        {id:'B388',name:'Appy', category:'Soft Drink', price:35, quantity:9}
+    ],
+    choco: [
+        {id:'A742',name:'5Star', category:'Chocloate', price:10, quantity:3},
+        {id:'A899',name:'Gems', category:'Chocloate', price:12, quantity:11},
+        {id:'B635',name:'KitKat', category:'Chocloate', price:15, quantity:7},
+        {id:'B408',name:'Perk', category:'Chocloate', price:8, quantity:15},
+        {id:'A354',name:'Dairy Milk', category:'Chocloate', price:30, quantity:4}
+    ]
+}
+let div = document.createElement('div');
+div.classList.add('block-holder');
+let product = document.createElement('div');
+product.classList.add('product');
+let list = document.createElement('div');
+list.classList.add('list');
+let info = document.createElement('div');
+info.classList.add('info');
+document.body.appendChild(div);
+div.append(product, list, info);
+let orderConteiner = document.createElement('div');
+let clearbtn = document.createElement('button')
+clearbtn.textContent = 'Delate'
+
+
+let  ul = `
+    <ul>
+        <li><button class="btn" data-data-id="drink">drink</button></li>
+        <li><button class="btn" data-data-id="choco">choco</button></li>
+        <li><button class="order" id="my-orders">My orders</button></li>
+    </ul>
+`;
+product.innerHTML = ul;
+function clear() {
+    location.reload()
+}
+
+
+let buttons = document.querySelectorAll('.btn');
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        orderConteiner. textContent = '';
+        let productId = button.dataset.dataId;
+        let productList = data[productId];
+        let productNames = productList.map(product => product.name);
+        list.textContent = '';
+        productNames.forEach(name => {
+            let li = document.createElement('li');
+            li.textContent = name;
+            list.appendChild(li);
+            let buyButton = document.createElement('button');
+            buyButton.textContent = 'Купить';
+            buyButton.addEventListener('click', () => {
+                let productInfo = productList.find(product => product.name === name);
+                alert(`Вы купили ${productInfo.name}!`);
+                const currentTime = new Date();
+                localStorage.setItem(`${name} ${currentTime} `, JSON.stringify(productInfo));//Добавляем в LocalStorage
+                clear()
+            });
+            let card = document.createElement('div');
+            card.classList.add('product-card');
+            card.innerHTML = `
+            <h2>${name}</h2>
+            <p>ID:${productList.find(product => product.name === name).id}
+            <p>Count: ${productList.find(product => product.name === name).price}</p>
+            <p>Quality: ${productList.find(product => product.name === name).quantity}</p>
+            `;
+            card.appendChild(buyButton);
+            li.addEventListener('click', () => {
+                info.textContent = '';
+                info.appendChild(card);
+            });
+        });
+    });
+})
+
+let showOrders = function() {
+    orderConteiner.textContent = ''
+    list.textContent = ''
+    info.textContent = ''
+    for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i);
+        orderConteiner.innerHTML += `<p>${key}: ${localStorage.getItem(key)}</p>`;//Выводим при нажатии на кнопку My orders
+        document.body.append(orderConteiner)
+    }
+    orderConteiner.appendChild(clearbtn);
+}
+
+let cleraCart = function() {
+    orderConteiner.textContent = ''
+    localStorage.clear()                           //очищаем корзину и локал Сторедж
+}
+
+let order = document.getElementById('my-orders');
+order.addEventListener('click', showOrders)
+clearbtn.addEventListener('click', cleraCart)
